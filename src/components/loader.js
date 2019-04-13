01ceed7f13dd4d867748ea6d6ca260a5bbc5b690
Stephano Paraskeva <stephanokparaskeva@gmail.com>
2019-04-13 11:24:18 +0100

    fixed loader.

--- src/components/loader.js
+++ src/components/loader.js
@@ -10,7 +10,6 @@ const Loader = () => {
   <path id="outline-bg" opacity="0.05" fill="none" stroke="#ededed" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10"
         d="				M93.9,46.4c9.3,9.5,13.8,17.9,23.5,17.9s17.5-7.8,17.5-17.5s-7.8-17.6-17.5-17.5c-9.7,0.1-13.3,7.2-22.1,17.1 				c-8.9,8.8-15.7,17.9-25.4,17.9s-17.5-7.8-17.5-17.5s7.8-17.5,17.5-17.5S86.2,38.6,93.9,46.4z" />
 				</svg>
-<a href="https://codepen.io/suez/pen/ogmMOM" target="_blank" className="check-out">Jump loader</a>
     </div>
   )
 }
