cd942233f86ef4dc545bb011f8e00d0c2eef4eb2
Stephen Sabo <sabo.stephen@gmail.com>
2019-04-15 11:57:54 +0100

    Update buy form styles

--- src/components/transactions/transactionForm.js
+++ src/components/transactions/transactionForm.js
@@ -22,11 +22,13 @@ class TransactionForm extends React.Component {
       },
       date: new Date(),
       isHidden: true,
+      calenderOpen: false,
       errors: {}
     }
 
     this.handleChange = this.handleChange.bind(this)
     this.handleDate = this.handleDate.bind(this)
+    this.toggleCalendar = this.toggleCalendar.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.toggleBuy = this.toggleBuy.bind(this)
     this.toggleSell = this.toggleSell.bind(this)
@@ -89,7 +91,14 @@ class TransactionForm extends React.Component {
     this.setState({
       data, date
     })
+    this.toggleCalendar()
   }
+
+  toggleCalendar (e) {
+    e && e.preventDefault()
+    this.setState({calenderOpen: !this.state.calenderOpen})
+  }
+
 
   checkTransactions(sellQuantity) {
     const filtered = this.state.transactions.filter(transaction => transaction.coin !== null && transaction.coin.currency === this.props.location.state.coin.currency)
@@ -132,21 +141,23 @@ class TransactionForm extends React.Component {
     const coin = this.props.location.state.coin
     const date = this.state.date
     return(
-      <div className="container u-full-width">
-        <div className="row">
-          <p>{coin.currency}/USD</p>
+      <div className="container">
+        <h4>{coin.currency}</h4>
+        <div className="row container">
+          <button onClick={ this.toggleBuy } className="six columns">BUY</button>
+          <button onClick={ this.toggleSell } className="six columns">SELL</button>
         </div>
-        <button onClick={ this.toggleBuy }>BUY</button>
-        <button onClick={ this.toggleSell }>SELL</button>
         {this.state.isHidden &&
           <BuyForm
             className="twelve columns"
             handleChange={this.handleChange}
             handleSubmit={this.handleSubmit}
+            toggleCalendar={this.toggleCalendar}
             handleDate={this.handleDate}
             data={this.state.data}
             coin={coin}
             date={date}
+            calenderOpen={this.state.calenderOpen}
           />
         }
         {!this.state.isHidden &&
