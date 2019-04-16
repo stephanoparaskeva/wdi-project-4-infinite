d73f0e0200f76ebdd85278cce36400e408d44ee1
Stephen Sabo <sabo.stephen@gmail.com>
2019-04-16 18:54:57 +0100

    Update coin show

--- src/components/coinShow.js
+++ src/components/coinShow.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Candle from '../candle'
 import Common from '../lib/common'
+import numeral from 'numeral'
 
 class Coin extends React.Component {
   constructor() {
@@ -18,10 +19,10 @@ class Coin extends React.Component {
         <div className="row">
           <h1>{coin.currency}</h1>
           <div className="row coin-info">
-            <span className="twelve columns">Price: ${parseFloat(coin.price).toFixed(2)}</span>
-            <span className={`${Common.checkChange(coin)} twelve columns`}>Price Change: {coin['1d'].price_change_pct}%</span>
-            <span className="twelve columns">Market Cap: ${coin.market_cap}</span>
-            <span className="twelve columns">Circulating Supply: {coin.circulating_supply} {coin.currency}</span>
+            <span className="twelve columns">Price: {numeral(parseFloat(coin.price)).format('$ 0,0.00') || 0}</span>
+            <span className={`${Common.checkChange(coin)} twelve columns`}>{coin['1d'].price_change_pct}%</span>
+            <span className="twelve columns">Market Cap: {numeral(coin.market_cap).format('($ 0.00 a)') || 0}</span>
+            <span className="twelve columns">Circulating Supply: {numeral(coin.circulating_supply).format('(0.00 a)') || 0}</span>
           </div>
         </div>
         <div className="row candle-graph">
